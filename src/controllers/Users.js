a1acc224ba2fac6f1235100dc3af0fd4c390a656
agusb19 <agustinbrage19@gmail.com>
2023-10-05 16:10:25 -0300

    feat: add validation data in users controller

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,55 +1,89 @@
 const asyncErrorHandler = require('../utils/errorHandler')
+const userValidation = require('../validations/Users')
 
 class Users {
     constructor({ userModel }) {
         this.userModel = userModel
+        this.validateUser = new userValidation()
     }
 
     getUser = asyncErrorHandler(async (req, res, next) => {
-        const { user } = req.query
+        // const { user } = req.query
+        const validation = this.validateUser.name(req.query)
 
-        const userName = await this.userModel.getUser({ user })
-
-        res.satus(200).json({
-            success: 'success',
-            data: {
-                userName
-            }
-        })
+        if(validation.success) {
+            const userName = await this.userModel.getUser(validation.data)
+    
+            res.satus(200).json({
+                success: 'success',
+                data: {
+                    userName
+                }
+            })
+        } else {
+            res.status(400).json({
+                success: 'error',
+                validationError: validation.error
+            })
+        }
     })
     
     getUserPassword = asyncErrorHandler(async (req, res, next) => {
-        const { user, password } = req.query
+        // const { user, password } = req.query
+        const validation = this.validateUser.nameAndPassword(req.query)
 
-        const userName = await this.userModel.getUserPassword({ user, password })
-
-        res.satus(200).json({
-            success: 'success',
-            data: {
-                userName
-            }
-        })
+        if(validation.success) {
+            const userName = await this.userModel.getUserPassword(validation.data)
+    
+            res.satus(200).json({
+                success: 'success',
+                data: {
+                    userName
+                }
+            })
+        } else {
+            res.status(400).json({
+                success: 'error',
+                validationError: validation.error
+            })
+        }
     })
 
     insertNewUser = asyncErrorHandler(async (req, res, next) => {
-        const { user, password } = req.body
+        // const { user, password } = req.body
+        const validation = this.validateUser.nameAndPassword(req.query)
 
-        const result = await this.userModel.insertNewUser({ user, password })
-
-        res.status(200).json({
-            success: 'success',
-            result
-        })
+        if(validation.success) {
+            const result = await this.userModel.insertNewUser(validation.data)
+    
+            res.status(200).json({
+                success: 'success',
+                result
+            })
+        } else {
+            res.status(400).json({
+                success: 'error',
+                validationError: validation.error
+            })
+        }
     })
 
     deleteUser = asyncErrorHandler(async (req, res, next) => {
-        const { user, password } = req.body
+        // const { user, password } = req.body
+        const validation = this.validateUser.nameAndPassword(req.query)
 
-        const result = await this.userModel.deleteUser({ user, password })
-
-        res.status(200).json({
-            success: 'success',
-            result
-        })
+        if(validation.success) {
+            const result = await this.userModel.deleteUser(validation.data)
+    
+            res.status(200).json({
+                success: 'success',
+                result
+            })
+        } else {
+            res.status(400).json({
+                success: 'error',
+                validationError: validation.error
+            })
+        }
     })
 }
