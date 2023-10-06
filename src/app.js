2c8b569b800e384be7e70f841f382b590ea0467a
agusb19 <agustinbrage19@gmail.com>
2023-10-06 11:35:04 -0300

    feat: error middleware implemented

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const { PORT } = require('./utils/config')
 const createUserRouter = require('./routes/users')
 const createArticleRouter = require('./routes/articles')
 const corsMiddleware = require('./middlewares/cors')
+const errorMiddleware = require('./middlewares/Error')
 
 const createApp = ({ userModel, articleModel }) => {
     const app = express()
     
     app.use(express.json())
     app.use(corsMiddleware())
+    app.use(errorMiddleware())
     app.disable('x-powered-by')
     
     app.use('./blogApi/user', createUserRouter({ userModel }))
