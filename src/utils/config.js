b7ca6a11070ee1dba280932866d869c09acb433d
agusb19 <agustinbrage19@gmail.com>
2023-10-06 01:04:33 -0300

    refactor: change import module in config util file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2')
+const { createPool } = require('mysql2/promise')
 const dotenv = require('dotenv')
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
-const pool = mysql.createPool({
+const pool = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
