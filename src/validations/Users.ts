1a5e85aa5688ba4d30b646d902c7afa59ee5a032
agusb19 <agustinbrage19@gmail.com>
2023-10-08 02:06:27 -0300

    refactor: change users validations according to schema

--- src/validations/Users.ts
+++ src/validations/Users.ts
@@ -1,26 +1,31 @@
-import { SafeParseReturnType } from 'zod';
-import { userSchema, userPasswordSchema } from '../schemas/users'
-import { type userType, type userPasswordType } from '../types/users';
+import { SafeParseReturnType } from 'zod'
+import { UserType } from '../types/users'
+import { userSchema } from '../schemas/users'
 
 export interface IUsersValidation {
-    name(data: unknown): SafeParseReturnType<unknown, userType>
-    nameAndPassword(data: unknown): SafeParseReturnType<unknown, userPasswordType>
+    id(data: unknown): SafeParseReturnType<unknown, UserType['id']>
+    name(data: unknown): SafeParseReturnType<unknown, UserType['name']>
+    data(data: unknown): SafeParseReturnType<unknown, UserType['data']>
+    idName(data: unknown): SafeParseReturnType<unknown, UserType['idName']>
+    idEmail(data: unknown): SafeParseReturnType<unknown, UserType['idEmail']>
+    idPhone(data: unknown): SafeParseReturnType<unknown, UserType['idPhone']>
+    idAuthor(data: unknown): SafeParseReturnType<unknown, UserType['idAuthor']>
+    idPassword(data: unknown): SafeParseReturnType<unknown, UserType['idPassword']>
 }
 
 export class UsersValidation implements IUsersValidation {
     private userSchema: typeof userSchema
-    private userPasswordSchema: typeof userPasswordSchema
 
     constructor() {
         this.userSchema = userSchema
-        this.userPasswordSchema = userPasswordSchema
     }
 
-    name(data: unknown) {
-        return this.userSchema.safeParse(data)
-    }
-
-    nameAndPassword(data: unknown) {
-        return this.userPasswordSchema.safeParse(data)
-    }
+    id = (data: unknown) => this.userSchema.id.safeParse(data)
+    name = (data: unknown) => this.userSchema.name.safeParse(data)
+    data = (data: unknown) => this.userSchema.data.safeParse(data)
+    idName = (data: unknown) => this.userSchema.idName.safeParse(data)
+    idEmail = (data: unknown) => this.userSchema.idEmail.safeParse(data)
+    idPhone = (data: unknown) => this.userSchema.idPhone.safeParse(data)
+    idAuthor = (data: unknown) => this.userSchema.idAuthor.safeParse(data)
+    idPassword = (data: unknown) => this.userSchema.idPassword.safeParse(data)
 }
