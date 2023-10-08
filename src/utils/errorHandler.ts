2bbc18ac4cbfe3581737f28fa857323d0b6edb78
agusb19 <agustinbrage19@gmail.com>
2023-10-08 18:17:10 -0300

    feat: add Error and Ok responses types

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,13 +1,35 @@
 import { Request, Response, NextFunction } from "express"
 import { RowDataPacket } from "mysql2" 
+import { ZodFormattedError } from "zod"
 
-export type AsyncFuntion = (req: Request, res: Response, next: NextFunction) => Promise<void>
-type ErrorHandler = (func: AsyncFuntion) => (req: Request, res: Response, next: NextFunction) => Promise<void>  
+type ErrorResponse = {
+    status: 'error',
+    validationError: ZodFormattedError<unknown, string>
+}
+
+type OkResponse = {
+    status: 'error' | 'success'
+    message?: string
+    data?: RowDataPacket[]
+}
+
+export type AsyncFuntion = (
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+) => Promise<Response<ErrorResponse | OkResponse> | void> 
+
+type ErrorHandler = (func: AsyncFuntion) => (
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+) => Promise<Response<ErrorResponse | OkResponse> | void> 
 
 export const asyncErrorHandler: ErrorHandler = (func) => {
     return async (req, res, next) => {
         try {
-            await func(req, res, next)
+            const response = await func(req, res, next)
+            // return response as Response<ErrorResponse | OkResponse>
         } catch (err) {
             next(err)
         }
