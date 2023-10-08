8d03df19c5279b378dd20356a37e7c3e5bb726eb
agusb19 <agustinbrage19@gmail.com>
2023-10-08 19:23:50 -0300

    feat: add all controllers methods to users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,23 @@
-import { Router } from '../controllers/Users'
-import UserController from '../controllers/Users'
+import { Router } from 'express'
+import { Users as UserController } from '../controllers/Users'
+import { type IUser } from '../types/users'
 
-const createUserRouter = ({ userModel }) => {
+const createUserRouter = ({ userModel }: { userModel: IUser }) => {
     const userRouter = Router()
 
     const userController = new UserController({ userModel })
 
-    userRouter.get('/', userController.getUserPassword)
-    userRouter.post('/', userController.insertNewUser)
-    userRouter.delete('/', userController.deleteUser)
+    userRouter.get('/all', userController.getAll)
+    userRouter.get('/validate', userController.getPassword)
+
+    userRouter.patch('/name', userController.changeName)
+    userRouter.patch('/email', userController.changeEmail)
+    userRouter.patch('/phone', userController.changePhone)
+    userRouter.patch('/author', userController.changeAuthor)
+    userRouter.patch('/password', userController.changePassword)
+
+    userRouter.post('/', userController.addNew)
+    userRouter.delete('/', userController.remove)
 
     return userRouter
 }
