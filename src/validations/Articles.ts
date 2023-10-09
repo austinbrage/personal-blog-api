b9d654c59f64d8920e760604af247a39d7d9b576
agusb19 <agustinbrage19@gmail.com>
2023-10-09 14:12:37 -0300

    feat: create article validations for controller

--- src/validations/Articles.ts
+++ src/validations/Articles.ts
@@ -1,77 +1,25 @@
 import { SafeParseReturnType } from 'zod'
-import { articleSchema, articleNameChangeSchema } from '../schemas/articles'
-import type { 
-    idType, 
-    idPostType, 
-    sectionType, 
-    allArticleType, 
-    publishStateType,
-    partialArticleType,
-    articleNameChangeType 
-} from '../types/articles'
+import { articleSchema } from '../schemas/articles'
+import { type ArticleType } from '../types/articles'
 
 export interface IArticlesValidation {
-    all(data: unknown): SafeParseReturnType<unknown, allArticleType>
-    partial(data: unknown): SafeParseReturnType<unknown, partialArticleType>
-    id(data: unknown): SafeParseReturnType<unknown, idType>
-    idPost(data: unknown): SafeParseReturnType<unknown, idPostType>
-    section(data: unknown): SafeParseReturnType<unknown, sectionType>
-    publishState(data: unknown): SafeParseReturnType<unknown, publishStateType>
-    nameChange(data: unknown): SafeParseReturnType<unknown, articleNameChangeType>
+    id(data: unknown): SafeParseReturnType<unknown, ArticleType['id']>
+    userId(data: unknown): SafeParseReturnType<unknown, ArticleType['userId']>
+    idName(data: unknown): SafeParseReturnType<unknown, ArticleType['idName']>
+    userIdName(data: unknown): SafeParseReturnType<unknown, ArticleType['userIdName']>
+    idPublishState(data: unknown): SafeParseReturnType<unknown, ArticleType['idPublishState']>
 }
 
 export class ArticlesValidation implements IArticlesValidation {
     private articleSchema: typeof articleSchema
-    private articleNameChangeSchema: typeof articleNameChangeSchema
-    
+
     constructor() {
         this.articleSchema = articleSchema
-        this.articleNameChangeSchema = articleNameChangeSchema
     }
 
-    all(data: unknown) {
-        return this.articleSchema.safeParse(data)
-    }
-
-    partial(data: unknown) {
-        return this.articleSchema.omit({ 
-            isPublish: true 
-        }).safeParse(data)
-    }
-
-    id(data: unknown) {
-        return this.articleSchema.pick({ 
-            id: true 
-        }).safeParse(data)
-    }
-    
-    idPost(data: unknown) {
-        return this.articleSchema.pick({ 
-            id: true, 
-            post: true 
-        }).safeParse(data)
-    }
-
-    section(data: unknown) {
-        return this.articleSchema.pick({ 
-            id: true, 
-            post: true, 
-            content: true, 
-            order: true 
-        }). safeParse(data)
-    }
-
-    publishState(data: unknown) {
-        return this.articleSchema.pick({ 
-            id: true, 
-            post: true, 
-            isPublish: true 
-        }).safeParse(data)
-    }
-
-    nameChange(data: unknown) {
-        return this.articleSchema.pick({
-            id: true
-        }).merge(this.articleNameChangeSchema).safeParse(data)
-    }
+    id = (data: unknown) => this.articleSchema.id.safeParse(data)
+    userId = (data: unknown) => this.articleSchema.userId.safeParse(data)
+    idName = (data: unknown) => this.articleSchema.idName.safeParse(data)
+    userIdName = (data: unknown) => this.articleSchema.userIdName.safeParse(data)
+    idPublishState = (data: unknown) => this.articleSchema.idPublishState.safeParse(data)
 }
