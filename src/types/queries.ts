99591c224559190dbd031aeaab9bc2fe44448afc
agusb19 <agustinbrage19@gmail.com>
2023-10-09 00:18:37 -0300

    feat: add queries enums for queries object util

---
+++ src/types/queries.ts
@@ -0,0 +1,32 @@
+export enum UserQueries {
+    getAll = 'getAll',
+    getPassword = 'getPassword',
+    addNew = 'addNew',
+    changeName = 'changeName',
+    changePassword = 'changePassword',
+    changeAuthor = 'changeAuthor',
+    changeEmail = 'changeEmail',
+    changePhone = 'changePhone',
+    remove = 'remove',
+}
+
+export enum ArticleQueries {
+    getAll = 'getAll',
+    getId = 'getId',
+    addNew = 'addNew',
+    changeName = 'changeName',
+    changePublishment = 'changePublishment',
+    remove = 'remove',
+}
+
+export enum SectionQueries {
+    getAll = 'getAll',
+    addNew = 'addNew',
+    changeContent = 'changeContent',
+    remove = 'remove',
+}
+
+export enum StyleQueries {
+    addNew = 'addNew',
+    changeAll = 'changeAll',
+}
