6f5ccb0cdb54a8f64f73fa41f452dffdb6aaa56a
agusb19 <agustinbrage19@gmail.com>
2023-10-09 19:32:25 -0300

    feat: add sections model and controller interfaces

--- src/types/sections.ts
+++ src/types/sections.ts
@@ -9,3 +9,17 @@ export type SectionType = {
     idContent: z.infer<typeof sectionSchema.idContent>
     articleIdContent: z.infer<typeof sectionSchema.articleIdContent>
 }
+
+export interface ISection {
+    getAll({ article_id }: SectionType['articleId']): Promise<RowDataPacket[]>
+    changeContent({ id, content }: SectionType['idContent']): Promise<RowDataPacket[]>
+    addNew({ article_id, content }: SectionType['articleIdContent']): Promise<RowDataPacket[]>
+    remove({ id }: SectionType['id']): Promise<RowDataPacket[]>
+}
+
+export interface SectionController {
+    getAll: AsyncFuntion
+    changeContent: AsyncFuntion
+    addNew: AsyncFuntion
+    remove: AsyncFuntion
+}
