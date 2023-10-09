862bc20abaf2cdb555d77549b8a3142a277683d5
agusb19 <agustinbrage19@gmail.com>
2023-10-09 15:03:13 -0300

    feat: add createPoolConnection utility

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,12 +6,19 @@ config()
 
 export const PORT = process.env.PORT || 3000
 
-export const pool: Pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-})
+export const createPoolConnection = (wait: boolean, limit: number, queue: number) => {
+    const pool: Pool = createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        waitForConnections: wait,
+        connectionLimit: limit,
+        queueLimit: queue
+        // waitForConnections: true,
+        // connectionLimit: 10,
+        // queueLimit: 0
+    })
+
+    return pool
+}
