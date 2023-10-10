9e49ceb1ea15e5417f5cc2cf4b62f76358901f20
agusb19 <agustinbrage19@gmail.com>
2023-10-09 21:32:57 -0300

    feat: create section validation class and interface

---
+++ src/validations/Sections.ts
@@ -0,0 +1,23 @@
+import { sectionSchema } from "../schemas/sections"
+import { type SectionType } from "../types/sections"
+import { type SafeParseReturnType } from "zod"
+
+export interface ISectionsValidation {
+    id(data: unknown): SafeParseReturnType<unknown, SectionType['id']>
+    idData(data: unknown): SafeParseReturnType<unknown, SectionType['idData']>
+    articleId(data: unknown): SafeParseReturnType<unknown, SectionType['articleId']>
+    articleIdData(data: unknown): SafeParseReturnType<unknown, SectionType['articleIdData']>
+}
+
+export class SectionValidation implements ISectionsValidation {
+    private sectionSchema: typeof sectionSchema
+
+    constructor() {
+        this.sectionSchema = sectionSchema
+    }
+
+    id = (data: unknown) => this.sectionSchema.id.safeParse(data)
+    idData = (data: unknown) => this.sectionSchema.idData.safeParse(data)
+    articleId = (data: unknown) => this.sectionSchema.articleId.safeParse(data)
+    articleIdData = (data: unknown) => this.sectionSchema.articleIdData.safeParse(data)
+}
