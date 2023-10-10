4838cc44c511e26a87415187bc43eb95e5285cc2
agusb19 <agustinbrage19@gmail.com>
2023-10-10 01:53:52 -0300

    feat: create sections table controller

---
+++ src/controllers/Sections.ts
@@ -0,0 +1,101 @@
+import { AsyncFuntion, asyncErrorHandler } from "../utils/errorHandler"
+import { SectionValidation, type ISectionsValidation } from "../validations/Sections"
+import type { Request, Response } from 'express'
+import { type SectionController } from "../types/sections"
+import { type ISection } from "../types/sections"
+import { type IStyle } from "../types/styles"
+import { type ZodError } from "zod"
+
+type ModelsType = {
+    styleModel: IStyle
+    sectionModel: ISection
+}
+
+export class Sections implements SectionController {
+    private styleModel: IStyle
+    private sectionModel: ISection
+    private validateSection: ISectionsValidation
+
+    constructor({ sectionModel, styleModel }: ModelsType) {
+        this.styleModel = styleModel
+        this.sectionModel = sectionModel
+        this.validateSection = new SectionValidation()
+    }
+
+    private validationErr(res: Response, validationError: ZodError<unknown>) {
+        return res.status(400).json({
+            status: 'error',
+            validationError: validationError.format()
+        })
+    }
+
+    getAll = asyncErrorHandler(async (req: Request, res: Response) => {
+        // const { article_id } = req.query 
+        const validation = this.validateSection.articleId(req.query)
+        
+        if(!validation.success) return this.validationErr(res, validation.error)
+
+        const result = await this.sectionModel.getAll(validation.data)
+
+        return res.status(200).json({
+            status: 'success',
+            data: result
+        })
+    })
+
+    changeAll = asyncErrorHandler(async (req: Request, res: Response) => {
+        // const { id, content, fontSize, fontWeight, fontFamily, lineHeight, marginTop, textAlign`, textColor } = req.body
+        const validation = this.validateSection.idData(req.body) 
+
+        if(!validation.success) return this.validationErr(res, validation.error)
+
+        await this.sectionModel.changeContent(validation.data)
+
+        const changeStyleData = {
+            ...validation.data,
+            section_id: validation.data.id
+        }
+
+        await this.styleModel.changeStyles(changeStyleData)
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'Section content and styles changed successfully'
+        })
+    })
+
+    addNew = asyncErrorHandler(async (req: Request, res: Response) => {
+        // const { article_id, content, fontSize, fontWeight, fontFamily, lineHeight, marginTop, textAlign`, textColor } = req.body
+        const validation = this.validateSection.articleIdData(req.body)
+
+        if(!validation.success) return this.validationErr(res, validation.error)
+
+        const newIdSection = await this.sectionModel.addNew(validation.data)
+
+        const addNewStyles = {
+            ...validation.data,
+            section_id: newIdSection
+        }
+
+        await this.styleModel.addNew(addNewStyles)
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'New section content and styles changed successfully'
+        })
+    })
+
+    remove = asyncErrorHandler(async (req: Request, res: Response) => {
+        // const { id } = req.body
+        const validation = this.validateSection.id(req.body)
+
+        if(!validation.success) return this.validationErr(res, validation.error)
+
+        await this.sectionModel.remove(validation.data)
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'Section content and styles removed successfully'
+        })
+    })
+}
