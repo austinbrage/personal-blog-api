9f401632fd3d4535800d313d44980d04c2d0337e
agusb19 <agustinbrage19@gmail.com>
2023-10-10 18:50:02 -0300

    feat: add secrets for authentication, authorization

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,6 +5,8 @@ import { config } from 'dotenv'
 config()
 
 export const PORT = process.env.PORT || 3000
+export const JWT_EXPIRE = process.env.JWT_EXPIRE
+export const SECRET_KEY = process.env.SECRET_KEY
 
 type CreatePool = {
     waitForConnection: boolean, 
