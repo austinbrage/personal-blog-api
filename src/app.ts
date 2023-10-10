d955fbd49c00e27007996f4821694389d70e2e1d
agusb19 <agustinbrage19@gmail.com>
2023-10-10 02:10:35 -0300

    feat: add section controller to app main route

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,22 @@ import express, { json } from 'express'
 import { PORT } from './utils/config'
 import createUserRouter from './routes/users'
 import createArticleRouter from './routes/articles'
+import createSectionRouter from './routes/sections'
 import corsMiddleware from './middlewares/cors'
 import errorMiddleware from './middlewares/error'
 import { type IUser } from './types/users'
 import { type IArticle } from './types/articles'
+import { type ISection } from './types/sections'
+import { type IStyle } from './types/styles'
 
-type modelsType = {
+type ModelsType = {
     userModel: IUser,
+    styleModel: IStyle
     articleModel: IArticle
+    sectionModel: ISection
 }
 
-const createApp = ({ userModel, articleModel }: modelsType) => {
+const createApp = ({ userModel, articleModel, sectionModel, styleModel }: ModelsType) => {
     const app = express()
     
     app.use(json())
@@ -22,6 +27,7 @@ const createApp = ({ userModel, articleModel }: modelsType) => {
     
     app.use('./blogApi/user', createUserRouter({ userModel }))
     app.use('./blogApi/article', createArticleRouter({ articleModel }))
+    app.use('./blogApi/section', createSectionRouter({ sectionModel, styleModel }))
 
     app.listen(PORT, () => {
         console.log(`Server running on Port: ${PORT}`)
