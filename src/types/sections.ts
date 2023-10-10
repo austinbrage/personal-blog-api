44640b15ce8b6e3ce2bc3f37f6e236dab6700ad5
agusb19 <agustinbrage19@gmail.com>
2023-10-09 21:27:09 -0300

    feat: add section types for section validation

--- src/types/sections.ts
+++ src/types/sections.ts
@@ -5,8 +5,10 @@ import { type AsyncFuntion } from '../utils/errorHandler'
 
 export type SectionType = {
     id: z.infer<typeof sectionSchema.id>
+    idData: z.infer<typeof sectionSchema.idData>
     articleId: z.infer<typeof sectionSchema.articleId>
     idContent: z.infer<typeof sectionSchema.idContent>
+    articleIdData: z.infer<typeof sectionSchema.articleIdData>
     articleIdContent: z.infer<typeof sectionSchema.articleIdContent>
 }
 
