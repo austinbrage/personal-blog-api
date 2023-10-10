1c53493309a1a20f9da4f1fbc9d249c1956b1ad2
agusb19 <agustinbrage19@gmail.com>
2023-10-10 02:10:55 -0300

    feat: add section and style model to server file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import createApp from './app'
 import UserModel from './models/User'
+import StyleModel from './models/Style'
 import ArticleModel from './models/Article'
+import SectionModel from './models/Section'
 
 const userModel = new UserModel()
+const styleModel = new StyleModel()
 const articleModel = new ArticleModel()
+const sectionModel = new SectionModel()
 
-const app = createApp({ userModel, articleModel })
+const app = createApp({ userModel, articleModel, sectionModel, styleModel })
 
 export default app
