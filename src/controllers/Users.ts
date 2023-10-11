034aae968bd613188d3c1a921e370f010ffd9355
agusb19 <agustinbrage19@gmail.com>
2023-10-11 16:11:01 -0300

    feat: implement authentication in users controller

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,16 +1,23 @@
 import { asyncErrorHandler } from '../utils/errorHandler'
+import createAuthentication from '../auth/authentication'
 import { UsersValidation, type IUsersValidation } from '../validations/Users'
-import type { Request, Response } from "express"
+import type { NextFunction, Request, Response } from "express"
 import { type UserController } from '../types/users'
 import { type IUser } from '../types/users'
 import { type ZodError } from 'zod'
+import { register } from 'module'
 
 export class Users implements UserController {
+    private compareHash
+    private registerHash
     private userModel: IUser
     private validateUser: IUsersValidation
 
     constructor({ userModel }: { userModel: IUser }) {
+        const { compareHash, registerHash } = createAuthentication({ userModel })
         this.userModel = userModel
+        this.compareHash = compareHash
+        this.registerHash = registerHash
         this.validateUser = new UsersValidation()
     }
     
@@ -35,7 +42,7 @@ export class Users implements UserController {
         })
     })
     
-    getPassword = asyncErrorHandler(async (req: Request, res: Response) => {
+    getPassword = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction) => {
         // const { name, password } = req.query
         const validation = this.validateUser.namePassword(req.query)
 
@@ -43,19 +50,32 @@ export class Users implements UserController {
 
         const result = await this.userModel.getIdPassword(validation.data)
 
-        if(result.length === 0 || result[0].password !== validation.data.password) {
+        if(result.length === 0) {
             return res.status(401).json({
                 status: 'error',
-                message: result.length === 0 
-                    ? 'Incorrect username' 
-                    : 'Incorrect password'
+                message: 'Incorrect username'
             })
         }
 
-        return res.status(200).json({
-            status: 'success',
-            message: 'User validated successfully' 
-        })
+        const compareHashData = {
+            modelResult: { id: result[0].id, password: result[0].password },
+            validatedData: validation.data
+        }
+
+        return this.compareHash(compareHashData, res, next)
+        // if(result.length === 0 || result[0].password !== validation.data.password) {
+        //     return res.status(401).json({
+        //         status: 'error',
+        //         message: result.length === 0 
+        //             ? 'Incorrect username' 
+        //             : 'Incorrect password'
+        //     })
+        // }
+
+        // return res.status(200).json({
+        //     status: 'success',
+        //     message: 'User validated successfully' 
+        // })
     })
 
     changeName = asyncErrorHandler(async (req: Request, res: Response) => {
@@ -66,7 +86,7 @@ export class Users implements UserController {
 
         await this.userModel.changeName(validation.data)
 
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             message: 'User name changed successfully'
         })
@@ -80,7 +100,7 @@ export class Users implements UserController {
 
         await this.userModel.changeEmail(validation.data)
 
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             message: 'User email changed successfully'
         })
@@ -94,7 +114,7 @@ export class Users implements UserController {
 
         await this.userModel.changePhone(validation.data)
 
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             message: 'User phone changed successfully'
         })
@@ -108,7 +128,7 @@ export class Users implements UserController {
 
         await this.userModel.changeAuthor(validation.data)
 
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             message: 'User author changed successfully'
         })
@@ -122,13 +142,13 @@ export class Users implements UserController {
 
         await this.userModel.changePassword(validation.data)
 
-        res.status(200).json({
+        return res.status(200).json({
             status: 'success',
             message: 'User password changed successfully'
         })
     })  
 
-    addNew = asyncErrorHandler(async (req: Request, res: Response) => {
+    addNew = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction) => {
         // const { name, password, email, phone, author } = req.body
         const validation = this.validateUser.data(req.body) 
 
@@ -137,17 +157,18 @@ export class Users implements UserController {
         const result = await this.userModel.getIdPassword(validation.data)
 
         if(result.length === 0) {
-            res.status(401).json({
+            return res.status(401).json({
                 status: 'error',
                 message: 'Existing user'
             })    
-        } else {
-            await this.userModel.addNew(validation.data)
-            res.status(200).json({
-                status: 'success',
-                message: 'User added successfully'
-            })
         }
+
+        return this.registerHash(validation.data, res, next)
+        // await this.userModel.addNew(validation.data)
+        // res.status(200).json({
+        //     status: 'success',
+        //     message: 'User added successfully'
+        // })
     })  
 
     remove = asyncErrorHandler(async (req: Request, res: Response) => {
