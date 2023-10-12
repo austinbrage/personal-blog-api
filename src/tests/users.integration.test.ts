48c1c0af903ce57c6bbfcb3cf5435e13ebfa4873
agusb19 <agustinbrage19@gmail.com>
2023-10-11 23:42:53 -0300

    test: change test according to new users routes

--- src/tests/users.integration.test.ts
+++ src/tests/users.integration.test.ts
@@ -2,89 +2,133 @@ import request from 'supertest'
 import app from '../server'
 import { type UserType } from "../types/users"
 
-describe('User-Route: Validate log-in, Make registrations and delete counts', () => {
-    const RESOURCE = '/blogApi/user'
+let id: number
+let cookies: string
+const RESOURCE = '/personal-blog/user'
+
+describe.skip('Users Route: Test register and delete user', () => {
+
+    test('should register new user', async () => {
+        await request(app)
+            .post(`${RESOURCE}/register`)
+            .send({ 
+                name: 'Usuario1', 
+                password: '5678', 
+                email: 'myEmail@gmail.com',
+                phone: '1234-5678',
+                author: 'Jack Smith',
+            })
+            .expect(201)    
+    })
     
-    describe('Get Method on /user', () => {
+    test('should login and delete new user', async () => {
+        const response1 = await request(app)
+            .post(`${RESOURCE}/login`)
+            .send({ name: 'Usuario1', password: '5678' })
+            .expect(200)
+        cookies = response1.headers['set-cookie']
 
-        describe('Given both correct username and password', () => {
-            const userData: UserType['namePassword'] = { name: 'Usuario0', password: '1234' }
+        const response2 = await request(app)
+            .get(`${RESOURCE}/data`)
+            .set('Cookie', cookies)
+            .expect(200)
+        id = response2.body.data.id
 
-            test('should validate log-in', async () => {
-                const response = await request(app).get(RESOURCE).query(userData)
-                expect(response.statusCode).toBe(200)
-                expect(response.body.message).toBe('User validated successfully')
-            })
-        })
-        
-        describe('Given correct username and wrong password', () => {
-            const userData: UserType['namePassword'] = { name: 'Usuario0', password: '1235' }
-
-            test('should reject log-in', async () => {
-                const response = await request(app).get(RESOURCE).query(userData)
-                expect(response.statusCode).toBe(401)
-                expect(response.body.message).toBe('Incorrect password')
-            })
-        })
-        
-        describe('Given wrong username and correct password', () => {
-            const userData: UserType['namePassword'] = { name: 'usuario0', password: '1234' }
-
-            test('should reject log-in', async () => {
-                const response = await request(app).get(RESOURCE).query(userData)
-                expect(response.statusCode).toBe(401)
-                expect(response.body.message).toBe('Incorrect username')
-            })
-        })
-        
-        describe('Given both wrong username and password', () => {
-            const userData: UserType['namePassword'] = { name: 'usuario0', password: '1235' }
-
-            test('should reject log-in', async () => {
-                const response = await request(app).get(RESOURCE).query(userData)
-                expect(response.statusCode).toBe(401)
-                expect(response.body.message).toBe('Incorrect username and password')
-            })
-        })
+        await request(app)
+            .delete(`${RESOURCE}/data`)
+            .set('Cookie', cookies)
+            .send({ id: id })
+            .expect(200)
     })
 
+})
 
-    describe('Post Method on /user', () => {
+describe.skip('Users Route: Test successful and unseccessful login', () => {
 
-        describe('Given a new user and password', () => {
-            const userData: UserType['namePassword'] = { name: 'Usuario10', password: '1234' }
-
-            test('should create a new user', async () => {
-                const response = await request(app).post(RESOURCE).send(userData)
-                expect(response.statusCode).toBe(201)
-                expect(response.body.message).toBe('User added successfully')
-            })
-        })
-        
-        describe('Given a new user that already exists', () => {
-            const userData: UserType['namePassword'] = { name: 'Usuario10', password: '1234' }
-
-            test('should warn the potential user', async () => {
-                const response = await request(app).post(RESOURCE).send(userData)
-                expect(response.statusCode).toBe(401)
-                expect(response.body.message).toBe('Existing user')
-            })
-        })
-    
+    test('should reject login if username is incorrect', async () => {
+        await request(app)
+            .post(`${RESOURCE}/login`)
+            .send({ name: 'Usuarios0', password: '1234' })
+            .expect(401)
     })
 
-
-    describe('Delete Method on /user', () => {
-        
-        describe('Given an existing user with its password', () => {
-            const userData: UserType['namePassword'] = { name: 'Usuario10', password: '1234' }
-
-            test('should create a new user', async () => {
-                const response = await request(app).delete(RESOURCE).send(userData)
-                expect(response.statusCode).toBe(200)
-                expect(response.body.message).toBe('User removed successfully')
-            })
-        })
+    test('should reject login if password is incorrect', async () => {
+        await request(app)
+            .post(`${RESOURCE}/login`)
+            .send({ name: 'Usuario0', password: '1235' })
+            .expect(401)
     })
 
+    test('should reject login if username and password are incorrect', async () => {
+        await request(app)
+            .post(`${RESOURCE}/login`)
+            .send({ name: 'Usuarios0', password: '1235' })
+            .expect(401)
+    })
+
+    test('should accept login if username and password are correct', async () => {
+        await request(app)
+            .post(`${RESOURCE}/login`)
+            .send({ name: 'Usuario0', password: '1234' })
+            .expect(401)
+    })
+})
+
+describe.skip('Users Route: Test resources after a successful login', () => {
+   
+    beforeAll(async () => {
+        const existingUser: UserType['namePassword'] = { name: 'Usuario0', password: '1234' }
+
+        const response1 = await request(app)
+            .post(`${RESOURCE}/login`)
+            .send(existingUser)
+            .expect(200)
+        cookies = response1.headers['set-cookie']
+
+        const response2 = await request(app)
+            .get(`${RESOURCE}/data`)
+            .set('Cookie', cookies)
+            .expect(200)
+        id = response2.body.data.id
+    })
+
+    test('should PATCH the user email', async () => {
+        await request(app)
+            .patch(`${RESOURCE}/email`)
+            .set('Cookie', cookies)
+            .send({ id: id, email: 'fakeEmail@gmail.com' })
+            .expect(200)
+    })
+
+    test('should PATCH the user phone', async () => {
+        await request(app)
+            .patch(`${RESOURCE}/phone`)
+            .set('Cookie', cookies)
+            .send({ id: id, phone: '5678-1234' })
+            .expect(200)
+    })
+
+    test('should PATCH the user author', async () => {
+        await request(app)
+            .patch(`${RESOURCE}/author`)
+            .set('Cookie', cookies)
+            .send({ id: id, author: 'John Jackson' })
+            .expect(200)
+    })
+
+    test('should PATCH the user name', async () => {
+        await request(app)
+            .patch(`${RESOURCE}/name`)
+            .set('Cookie', cookies)
+            .send({ id: id, email: 'Usuario0' })
+            .expect(200)
+    })
+
+    test('should PATCH the user password', async () => {
+        await request(app)
+            .patch(`${RESOURCE}/name`)
+            .set('Cookie', cookies)
+            .send({ id: id, password: '1234' })
+            .expect(200)
+    })
 })
