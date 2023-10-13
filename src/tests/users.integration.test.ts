b5e958ee31dd15c24d4c6bd7dde5892cf90ff77b
agusb19 <agustinbrage19@gmail.com>
2023-10-13 03:30:03 -0300

    test: change and fix users routes test

--- src/tests/users.integration.test.ts
+++ src/tests/users.integration.test.ts
@@ -2,13 +2,12 @@ import request from 'supertest'
 import app from '../server'
 import { type UserType } from "../types/users"
 
-let id: number
 let cookies: string
 const RESOURCE = '/personal-blog/user'
 
 describe.skip('Users Route: Test register and delete user', () => {
 
-    test('should register new user', async () => {
+    test('should SIGN-UP new user', async () => {
         await request(app)
             .post(`${RESOURCE}/register`)
             .send({ 
@@ -21,23 +20,21 @@ describe.skip('Users Route: Test register and delete user', () => {
             .expect(201)    
     })
     
-    test('should login and delete new user', async () => {
+    test('should SIGN-IN and DELETE new user', async () => {
         const response1 = await request(app)
             .post(`${RESOURCE}/login`)
             .send({ name: 'Usuario1', password: '5678' })
             .expect(200)
         cookies = response1.headers['set-cookie']
 
-        const response2 = await request(app)
+        await request(app)
             .get(`${RESOURCE}/data`)
             .set('Cookie', cookies)
             .expect(200)
-        id = response2.body.data.id
 
         await request(app)
             .delete(`${RESOURCE}/data`)
             .set('Cookie', cookies)
-            .send({ id: id })
             .expect(200)
     })
 
@@ -45,32 +42,32 @@ describe.skip('Users Route: Test register and delete user', () => {
 
 describe.skip('Users Route: Test successful and unseccessful login', () => {
 
-    test('should reject login if username is incorrect', async () => {
+    test('should NOT LOGIN if username is incorrect', async () => {
         await request(app)
             .post(`${RESOURCE}/login`)
             .send({ name: 'Usuarios0', password: '1234' })
             .expect(401)
     })
 
-    test('should reject login if password is incorrect', async () => {
+    test('should NOT LOGIN if password is incorrect', async () => {
         await request(app)
             .post(`${RESOURCE}/login`)
             .send({ name: 'Usuario0', password: '1235' })
             .expect(401)
     })
 
-    test('should reject login if username and password are incorrect', async () => {
+    test('should NOT LOGIN if username and password are incorrect', async () => {
         await request(app)
             .post(`${RESOURCE}/login`)
             .send({ name: 'Usuarios0', password: '1235' })
             .expect(401)
     })
 
-    test('should accept login if username and password are correct', async () => {
+    test('should LOGIN if username and password are correct', async () => {
         await request(app)
             .post(`${RESOURCE}/login`)
             .send({ name: 'Usuario0', password: '1234' })
-            .expect(401)
+            .expect(200)
     })
 })
 
@@ -79,56 +76,91 @@ describe.skip('Users Route: Test resources after a successful login', () => {
     beforeAll(async () => {
         const existingUser: UserType['namePassword'] = { name: 'Usuario0', password: '1234' }
 
-        const response1 = await request(app)
+        const response = await request(app)
             .post(`${RESOURCE}/login`)
             .send(existingUser)
             .expect(200)
-        cookies = response1.headers['set-cookie']
-
-        const response2 = await request(app)
-            .get(`${RESOURCE}/data`)
-            .set('Cookie', cookies)
-            .expect(200)
-        id = response2.body.data.id
+        cookies = response.headers['set-cookie']
     })
 
-    test('should PATCH the user email', async () => {
+    test('should PATCH the user data', async () => {
         await request(app)
             .patch(`${RESOURCE}/email`)
             .set('Cookie', cookies)
-            .send({ id: id, email: 'fakeEmail@gmail.com' })
+            .send({ email: 'fakeEmail@gmail.com' })
             .expect(200)
-    })
 
-    test('should PATCH the user phone', async () => {
         await request(app)
             .patch(`${RESOURCE}/phone`)
             .set('Cookie', cookies)
-            .send({ id: id, phone: '5678-1234' })
+            .send({ phone: '5678-1234' })
             .expect(200)
-    })
 
-    test('should PATCH the user author', async () => {
         await request(app)
             .patch(`${RESOURCE}/author`)
             .set('Cookie', cookies)
-            .send({ id: id, author: 'John Jackson' })
+            .send({ author: 'John Jackson' })
             .expect(200)
-    })
-
-    test('should PATCH the user name', async () => {
+        
         await request(app)
             .patch(`${RESOURCE}/name`)
             .set('Cookie', cookies)
-            .send({ id: id, email: 'Usuario0' })
+            .send({ name: 'Usuario0' })
+            .expect(200)
+
+        await request(app)
+            .patch(`${RESOURCE}/password`)
+            .set('Cookie', cookies)
+            .send({ password: '1234' })
             .expect(200)
     })
+})
 
-    test('should PATCH the user password', async () => {
+describe.skip('Users Route: Test resources with bad requests', () => {
+   
+    beforeAll(async () => {
+        const existingUser: UserType['namePassword'] = { name: 'Usuario0', password: '1234' }
+
+        const response = await request(app)
+            .post(`${RESOURCE}/login`)
+            .send(existingUser)
+            .expect(200)
+        cookies = response.headers['set-cookie']
+    })
+
+    test('should DENY bad requests', async () => {
+        await request(app)
+            .post(`${RESOURCE}/login`)
+            .send({ name: 'Usuario0' })
+            .expect(400)
+
+        await request(app)
+            .patch(`${RESOURCE}/email`)
+            .set('Cookie', cookies)
+            .send({ emails: 'fakeEmail@gmail.com' })
+            .expect(400)
+            
+        await request(app)
+            .patch(`${RESOURCE}/phone`)
+            .set('Cookie', cookies)
+            .send({ phone: 56781234 })
+            .expect(400)
+
+        await request(app)
+            .patch(`${RESOURCE}/author`)
+            .set('Cookie', cookies)
+            .send({ author: true })
+            .expect(400)
+
         await request(app)
             .patch(`${RESOURCE}/name`)
             .set('Cookie', cookies)
-            .send({ id: id, password: '1234' })
-            .expect(200)
+            .send({ email: 'Usuario0' })
+            .expect(400)
+
+        await request(app)
+            .patch(`${RESOURCE}/password`)
+            .set('Cookie', cookies)
+            .expect(400)
     })
 })
