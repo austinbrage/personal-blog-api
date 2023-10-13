86537dcfba4fa00cf6b1f90175561acaf63d5d1c
agusb19 <agustinbrage19@gmail.com>
2023-10-13 15:50:02 -0300

    feat: create app responses generator functions

---
+++ src/utils/appResponse.ts
@@ -0,0 +1,40 @@
+import { RowDataPacket } from "mysql2"
+import type { OkResponse, ErrorResponse } from "../types/custom"
+import { ZodFormattedError } from "zod"
+
+export const createOkResponse = ({
+    message, 
+    data
+}: {
+    message: string, 
+    data?: RowDataPacket[]
+}
+): OkResponse => {
+
+    return {
+        success: true,
+        result: {
+            message: message,
+            data: data ? data : null
+        }
+    }
+}
+
+export const createErrorResponse = ({
+    message,
+    error
+}: {
+    message: string, 
+    error?: ZodFormattedError<unknown>
+}
+): ErrorResponse => {
+
+    return {
+        success: false,
+        error: {
+            status: 'fail',
+            message: message,
+            validationError: error ? error : null
+        }
+    }
+}
