c05ac83cfd8dcf969a9a9ce2e07225c0148f1f5a
agusb19 <agustinbrage19@gmail.com>
2023-10-18 00:51:00 -0300

    feat: add backup message in error middleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,19 +2,20 @@ import { ErrorRequestHandler } from "express"
 import { type ErrorResponse } from "../types/custom"
 
 const globalErrorMiddleware: ErrorRequestHandler = (error, _req, res, _next) => {
-    error.statusCode = error.statusCode || 500 
-    error.status = error.status || 'error'
+    const message = error.message || 'Internal Server Error'
+    const statusCode = error.statusCode || 500 
+    const status = error.status || 'error'
     
     const response: ErrorResponse = {
         success: false,
         error: {
-            status: error.status,
-            message: error.message,
+            status: status,
+            message: message,
             validationError: null
         }
     }
 
-    return res.status(error.statusCode).json(response)
+    return res.status(statusCode).json(response)
 }
 
 export default globalErrorMiddleware
