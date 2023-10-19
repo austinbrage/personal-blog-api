e5f13272587390776ec64733c6f8a42a990eb1ac
agusb19 <agustinbrage19@gmail.com>
2023-10-19 19:32:07 -0300

    fix: release db connection

--- src/routes/connection.ts
+++ src/routes/connection.ts
@@ -7,13 +7,17 @@ const connectionRouter = Router()
 const pool = createPoolConnection()
 
 connectionRouter.get('/', async (_req: Request, res: Response) => {
+    const connection = await pool.getConnection()
+    
     try {
-        const connection = await pool.getConnection()
         await connection.query('SELECT 1')
         res.status(200).send('pong')      
         
     } catch(err) {
         res.status(500).send(`Database connection error (${err})`)
+
+    } finally {
+        connection.release()
 
     }
 })
