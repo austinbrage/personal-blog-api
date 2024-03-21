92c3754d6774aa5e5bb0020d8f19b09505d1f659
agusb19 <agustinbrage19@gmail.com>
2024-03-21 20:46:51 -0300

    feat: add s3 bucket config object on config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -9,6 +9,13 @@ type DBConfig = {
     database?: string
 }
 
+type BucketConfig = {
+    accessKey?: string
+    bucketName?: string
+    bucketRegion?: string
+    secretAccessKey?: string
+}
+
 export const ENVIRONMENT = process.env.NODE_ENV ?? 'production'
 
 export const JWT_EXPIRE = process.env.JWT_EXPIRE
@@ -42,4 +49,25 @@ export const dbConfig: {[env: string]: DBConfig} = {
         password: process.env.TEST_DB_PASSWORD,
         database: process.env.TEST_DB_DATABASE,
     },
+}
+
+export const bucketConfig: {[env: string]: BucketConfig} = {
+    development: {
+        accessKey: process.env.DEV_ACCESS_KEY,
+        bucketName: process.env.DEV_BUCKET_NAME,
+        bucketRegion: process.env.DEV_BUCKET_REGION,
+        secretAccessKey: process.env.DEV_SECRET_ACCESS_KEY,
+    },
+    production: {
+        accessKey: process.env.PROD_ACCESS_KEY,
+        bucketName: process.env.PROD_BUCKET_NAME,
+        bucketRegion: process.env.PROD_BUCKET_REGION,
+        secretAccessKey: process.env.PROD_SECRET_ACCESS_KEY,
+    },
+    test: {
+        accessKey: process.env.TEST_ACCESS_KEY,
+        bucketName: process.env.TEST_BUCKET_NAME,
+        bucketRegion: process.env.TEST_BUCKET_REGION,
+        secretAccessKey: process.env.TEST_SECRET_ACCESS_KEY,
+    }
 }
