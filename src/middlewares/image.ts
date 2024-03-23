7fb924cf621da9d69e8ba618aa0ee8b68954f060
agusb19 <agustinbrage19@gmail.com>
2024-03-23 12:54:35 -0300

    feat: check req.file in middleware and not in controller

--- src/middlewares/image.ts
+++ src/middlewares/image.ts
@@ -6,15 +6,19 @@ const imageFileMiddleware: RequestHandler = (req, _res, next) => {
     const maxFileSize = 3 * 1024 * 1024 // 3 MB
     const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/webp']
 
-    if (req.file && !req.is('multipart/form-data')) {
+    if(!req.file) {
+        return next(new CustomError('Validation data error, image file required', 400))
+    }
+
+    if (!req.is('multipart/form-data')) {
         return next(new CustomError('Wrong header, must be multipart/form-data', 400))
     }
 
-    if (req.file && !allowedMimeTypes.includes(req.file.mimetype)) {
+    if (!allowedMimeTypes.includes(req.file.mimetype)) {
         return next(new CustomError('Wrong image format, must be WEBP, JPEG or PNG', 400))
     }
 
-    if (req.file && req.file.size > maxFileSize) {
+    if (req.file.size > maxFileSize) {
         return next(new CustomError('Wrong image size, must not exceed 3 MB', 400))
     }
 
